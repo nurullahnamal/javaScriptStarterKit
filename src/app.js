2d40b434a07d250494b230374522e46f38c9dc8f
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-15 13:24:24 +0300

    1.01 commint

--- src/app.js
+++ src/app.js
@@ -11,4 +11,15 @@ console.log(dolarDun)
 
 let konutKredisi=["Konut Kredisi","Emlak konut Kredisi","Kamu Konut Kredisi"]
 
+console.log("<ul>")
+
+for (let i = 0; i < 3; i++) {
+
+    console.log("<li>"+konutKredisi[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredisi)
 
