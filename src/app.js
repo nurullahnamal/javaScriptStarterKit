73757798b95dea2e22a9dbd628afc68e67863a5a
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-15 13:17:45 +0300

    1.commint

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhab Dünya")
+
+let  dolarDun=9.44
+
+let dolarBugun=9.30
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+
+let konutKredisi=["Konut Kredisi","Emlak konut Kredisi","Kamu Konut Kredisi"]
+
+
